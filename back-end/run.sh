32cbdb6efa90d5b723fac5834321579ef1db8fee
Lucas Oliveira <olivmath@protonmail.com>
2022-12-27 16:41:05 -0300

    update: scripts

--- back-end/run.sh
+++ back-end/run.sh
@@ -7,5 +7,6 @@
   --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
   --validator \
   --rpc-methods Unsafe \
-  --name MyNode01 \
+  --name MyNode02 \
+  --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/ \
   --password-interactive
