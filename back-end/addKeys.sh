77646ce5f950f3ef4ab72ae0c0cba1a7474f1b5d
Lucas Oliveira <olivmath@protonmail.com>
2022-12-27 16:26:04 -0300

    update: scripts

--- back-end/addKeys.sh
+++ back-end/addKeys.sh
@@ -1,6 +1,6 @@
 ./target/release/node-template key insert \
-    --base-path /blockchain/node01 \
-  --chain customSpecRaw.json \
+  --base-path /blockchain/node01 \
+  --chain chainSpecRaw.json \
   --scheme Sr25519 \
   --suri <your-secret-seed> \
   --password-interactive \
@@ -8,7 +8,7 @@
 
 ./target/release/node-template key insert \
   --base-path /blockchain/node01 \
-  --chain customSpecRaw.json \
+  --chain chainSpecRaw.json \
   --scheme Ed25519 \
   --suri <your-secret-key> \
   --password-interactive \
